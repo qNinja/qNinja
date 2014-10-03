237259791d13e710d3aff1c424a77a64ab4f5efc
Mark N Broadhead <MarkNBroadhead@gmail.com>
2014-10-02 20:09:45 -0600

    added queue controller

---
+++ public/modules/queue/controllers/queue.client.controller.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('queue').controller('QueueController', ['$scope',
+	function($scope, $http) {
+		$scope.SRvisible = false;
+		//$http.get('http://localhost:8080/api/v1/SRs').
+		//$http.get('http://mbroadhead.lab.novell.com/api/v1/SRs').
+		$http.get('http://localhost:8080/api/v1/bigqueue').
+		success(function(data, status, headers, config) {
+			console.log('Getting SRs for Queue from API');
+			$scope.SRs = data;
+		}).
+		error(function(data, status, headers, config) {
+			console.log('Can\'t access API');
+		});
+
+		$scope.selectedSR = {};
+
+		$scope.selectSR = function(SR) {
+			$scope.selectedSR = SR;
+			$scope.SRvisible = true;
+		};
+	}
+]);
