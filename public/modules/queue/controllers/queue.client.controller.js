69c498754c7cabab003fc57227465156d9ceefe8
Mark N Broadhead <marknbroadhead@gmail.com>
2014-10-03 15:21:13 -0600

    spacing

--- public/modules/queue/controllers/queue.client.controller.js
+++ public/modules/queue/controllers/queue.client.controller.js
@@ -5,14 +5,14 @@ angular.module('queue').controller('QueueController', ['$scope',
 		$scope.SRvisible = false;
 		//$http.get('http://localhost:8080/api/v1/SRs').
 		//$http.get('http://mbroadhead.lab.novell.com/api/v1/SRs').
-		$http.get('http://localhost:8080/api/v1/bigqueue').
-		success(function(data, status, headers, config) {
-			console.log('Getting SRs for Queue from API');
-			$scope.SRs = data;
-		}).
-		error(function(data, status, headers, config) {
-			console.log('Can\'t access API');
-		});
+		.$http.get('http://localhost:8080/api/v1/bigqueue').
+			success(function(data, status, headers, config) {
+				console.log('Getting SRs for Queue from API');
+				$scope.SRs = data;
+			}).
+			error(function(data, status, headers, config) {
+				console.log('Can\'t access API');
+			});
 
 		$scope.selectedSR = {};
 
