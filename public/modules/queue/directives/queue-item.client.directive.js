105834b5bd283ae78a002bc97958670e77006076
Mark N Broadhead <marknbroadhead@gmail.com>
2014-10-03 22:24:06 -0600

    Changed from using template URL to include entire template in the directive. Will want to change this back for sure.

--- public/modules/queue/directives/queue-item.client.directive.js
+++ public/modules/queue/directives/queue-item.client.directive.js
@@ -3,15 +3,12 @@
 angular.module('queue').directive('queueItem', [
 	function() {
 		return {
-			templateURL: '../templates/queue-item.client.template.html',
 			restrict: 'AE',
 			replace: true,
-			link: function postLink(scope, element, attrs) {
-				// Queue item directive logic
-				// ...
-
-				element.text('this is the queueItem directive');
-			}
+			//templateURL: '../templates/queue-item.client.template.html'
+			// link: function postLink(scope, element, attrs) {
+			// }
+			template: '<div class="btn btn-default btn-block" data-ng-click="selectSR(SR)">{{ SR.SR_SEVERITY }} | {{ SR.SUPPORT_HOURS }}<ng-switch on="SR.RESPOND_VIA"> | <span ng-switch-when="Email"><span class="slyphicon glyphicon-envelope"></span></span><span ng-switch-when="Call"><span class="glyphicon glyphicon-earphone"></span></span><span ng-switch-when="Chat"><span class="glyphicon glyphicon-comment"></span</span><span ng-switch-default>None</span></ng-switch><span ng-if="SR.high_value"> <span class="glyphicon glyphicon-flag"></span></span><br />{{ SR.SR_BRIEF_DESC | cut:true:42:" ..." }}</div>'
 		};
 	}
 ]);
