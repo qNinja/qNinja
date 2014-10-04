0c06d85881c77e2dc7af47657eb2d6c9965ff884
Mark N Broadhead <marknbroadhead@gmail.com>
2014-10-04 09:17:51 -0600

    Fixed template linking behavior

--- public/modules/queue/directives/queue-item.client.directive.js
+++ public/modules/queue/directives/queue-item.client.directive.js
@@ -5,10 +5,9 @@ angular.module('queue').directive('queueItem', [
 		return {
 			restrict: 'AE',
 			replace: true,
-			//templateURL: '../templates/queue-item.client.template.html'
+			templateUrl: 'modules/queue/templates/queue-item.client.template.html'
 			// link: function postLink(scope, element, attrs) {
 			// }
-			template: '<div class="btn btn-default btn-block" data-ng-click="selectSR(SR)">{{ SR.SR_SEVERITY }} | {{ SR.SUPPORT_HOURS }}<ng-switch on="SR.RESPOND_VIA"> | <span ng-switch-when="Email"><span class="slyphicon glyphicon-envelope"></span></span><span ng-switch-when="Call"><span class="glyphicon glyphicon-earphone"></span></span><span ng-switch-when="Chat"><span class="glyphicon glyphicon-comment"></span</span><span ng-switch-default>None</span></ng-switch><span ng-if="SR.high_value"> <span class="glyphicon glyphicon-flag"></span></span><br />{{ SR.SR_BRIEF_DESC | cut:true:42:" ..." }}</div>'
 		};
 	}
 ]);
