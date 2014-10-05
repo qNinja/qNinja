5197b9e5abf16028a4c1179535d59f75eb2fbf30
Mark N Broadhead <marknbroadhead@gmail.com>
2014-10-05 16:50:23 -0600

    added mail module dependencies to the module registration

--- public/modules/mail/mail.client.module.js
+++ public/modules/mail/mail.client.module.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Use applicaion configuration module to register a new module
-ApplicationConfiguration.registerModule('mail');
+ApplicationConfiguration.registerModule('mail', ['ngGrid', 'LocalStorageModule', 'ui.bootstrap']);
