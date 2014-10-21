eb66e5fef48ce288f99c73c81ab5bcf85b52ed30
Mark N Broadhead <marknbroadhead@gmail.com>
2014-10-20 21:08:21 -0600

    Added filter "bySubscribedQueues" to queue module. This queue filters out all tickets not in the subscribedQueues array.

---
+++ public/modules/queue/filters/by-subscribed-queues.client.filter.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Filters out any Ticket the Agent is not responsible for. Takes an array of PseudoQueues as a parameter.
+angular.module('queue').filter('bySubscribedQueues', [function() {
+		return function(input, subscribedQueues) {
+			var result = [];
+			angular.forEach(input, function(Ticket) {
+				var isInSubscribedQueues = false;
+				for (var queue in subscribedQueues) {
+					console.log("looking for " + subscribedQueues[queue] + " in SR: " + Ticket.SR_NUM)
+					if (subscribedQueues[queue] === Ticket.PSEUDOQUEUE_ID) {
+						console.log("testing to see if " + subscribedQueues[queue] + " is the same as " + Ticket.PSEUDOQUEUE_ID + ".")
+						isInSubscribedQueues = true;
+					}
+				}
+				if (isInSubscribedQueues) {
+					result.push(Ticket);
+				}
+			});
+			return result;
+		};
+	}
+]);
