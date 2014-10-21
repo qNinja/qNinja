3912dff23d22b3f9b9d1ffcb15f89f5373cde1b8
Mark N Broadhead <MarkNBroadhead@gmail.com>
2014-10-21 14:21:12 -0600

    Moved some stuff around for readability

--- public/modules/queue/controllers/queue.client.controller.js
+++ public/modules/queue/controllers/queue.client.controller.js
@@ -3,18 +3,16 @@
 angular.module('queue').controller('QueueController', ['$scope', '$http',
 	function($scope, $http) {
 		$scope.SRvisible = false;
-		//$http.get('http://localhost:8080/api/v1/SRs').
-		//$http.get('http://mbroadhead.lab.novell.com/api/v1/SRs').
-		$http.get('http://localhost:3000/api/v1/bigqueue').
-			success(function(data, status, headers, config) {
+		$scope.selectedSR = {};
+
+		$http.get('http://localhost:3000/api/v1/bigqueue')
+			.success(function(data, status, headers, config) {
 				console.log('Getting SRs for Queue from API');
 				$scope.SRs = data;
-			}).
-			error(function(data, status, headers, config) {
+			})
+			.error(function(data, status, headers, config) {
 				console.log('Can\'t access API');
 			});
-
-		$scope.selectedSR = {};
 
 		$scope.selectSR = function(SR) {
 			$scope.selectedSR = SR;
