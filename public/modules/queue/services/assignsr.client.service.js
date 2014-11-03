4fb7ba08b884f226a662004da77442b80b4f197e
Mark N Broadhead <MarkNBroadhead@gmail.com>
2014-11-02 19:21:39 -0700

    changed backend to not actually assign Tickets, working on getting the assign SR button to have its intended functionality.

---
+++ public/modules/queue/services/assignsr.client.service.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('queue').factory('AssignSR', [
+	function($http, srNumber, owner) {
+		// Assignsr service logic
+		// TODO move elsewhere
+		var APIServer = 'http://localhost:3000/';
+		var RequestURL = APIServer + 'assignSR.asp?sr=' + srNumber + '&owner=' + owner;
+		alert('test');
+		// Public API
+		return {
+			assign: function($http) {
+				$http.patch(RequestURL)
+					.success(function(data, status, headers, config) {
+					// TODO Success Notification
+					return true;
+					})
+					.error(function(data, status, headers, config) {
+					// TODO Error Notification
+					return false;
+					});
+			}
+		};
+	}
+]);
