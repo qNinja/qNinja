7ae303eaf910248fb02a1c91f3db2265b856e145
Mark N Broadhead <marknbroadhead@gmail.com>
2014-11-07 00:25:55 -0700

    This commit does several things. I am assigning functionality to the assign button. I added a variable in the queue controller scope, currentUser to store the username for assigning SRs. It scrolls the screen back to the top when selecting an SR. I moved the APIServer variable out of hardcoded values in the controller. I set filterSubscribedQueues to default to true. Upon selecting an SR, the screen scrolls to the top. I also added more logging to assignSR and commented out the actual assigning part of the code so I can test.

--- public/modules/queue/controllers/queue.client.controller.js
+++ public/modules/queue/controllers/queue.client.controller.js
@@ -1,34 +1,61 @@
 'use strict';
 
-angular.module('queue').controller('QueueController', ['$scope', '$http',
-	function($scope, $http) {
-		// pretty sure these are unneeded
-		//$scope.SRvisible = false;
-		//$scope.selectedSR = {};
+angular.module('queue').controller('QueueController', ['$scope', '$http', '$interval', '$window',
+	function($scope, $http, $interval, $window) {
+		$scope.filterSubscribedQueues = true;
 
-		$http.get('http://localhost:3000/api/v1/SRs')
-			.success(function(data, status, headers, config) {
-				console.log('Getting SRs for Queue from API');
-				$scope.SRs = data;
-			})
-			.error(function(data, status, headers, config) {
-				console.log('Can\'t access API');
-			});
+		var APIServer = 'http://localhost:3000/';
+		$scope.currentUser = 'MBROADHEAD';
+		$scope.updateSRs = function() {
+			$http.get(APIServer + 'api/v1/SRs')
+				.success(function(data, status, headers, config) {
+					console.log('Getting SRs for Queue from API');
+					$scope.SRs = data;
+				})
+				.error(function(data, status, headers, config) {
+					console.log('Error getting SR list from API.');
+				});			
+		};
 
+		// Initial population of SR list
+		$scope.updateSRs();
+		// Check again every x milliseconds.
+		$scope.repeat = $interval(function(){ 
+			$scope.updateSRs();
+ 		},'10000');
+
+		// Selects the passed SR object and displays the information in SRInfo
 		$scope.selectSR = function(SR) {
 			$scope.selectedSR = SR;
 			$scope.SRvisible = true;
 
+			// scroll to top of page
+			$window.scroll(0, 0);
+
 			// update SR with more info
 			console.log('attempting to update SR');
-			$http.get('http://localhost:3000/api/v1/SRs/' + SR.sr_number)
+			$http.get(APIServer + 'api/v1/SRs/' + SR.sr_number)
 				.success(function(data, status, headers, config) {
 					console.log('Getting SR ' + SR.sr_number);
 					$scope.selectedSR = data;
 				})
 				.error(function(data, status, headers, config) {
-					console.log('Can\'t access API');
+					console.log('Error getting more information for SR# ' + SR.sr_number + '.');
 				});
+		};
+
+		$scope.assignSR = function(srNumber, owner) {
+			var RequestURL = APIServer + 'assignSR.asp?sr=' + srNumber + '&owner=' + owner;
+			console.log('Assigning SR# ' + srNumber + ' to ' + owner + '.');
+			// $http.patch(RequestURL)
+			// 	.success(function(data, status, headers, config) {
+			// 		// TODO Success Notification
+			// 	return true;
+			// 	})
+			// 	.error(function(data, status, headers, config) {
+			// 		// TODO Error Notification
+			// 	return false;
+			// 	});
 		};
 
 		$scope.subscribedQueues = [
