dc06ec885701619dff05a818aa10d97172d3f820
Mark N Broadhead <marknbroadhead@gmail.com>
2014-11-07 00:30:53 -0700

    going to start working on moving functionality from controller to services.

---
+++ public/modules/queue/services/getsrs.client.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('queue').factory('Getsrs', [
+	function() {
+		// Getsrs service logic
+		// ...
+
+		// Public API
+		return {
+			someMethod: function() {
+				return true;
+			}
+		};
+	}
+]);
