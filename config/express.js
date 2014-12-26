qNinja/qNinja

commit 4ebe829640a358382fff7a574caa0106db640897

connect mongo causing issues, disabling for now

config/express.js
@@ -85,16 +85,19 @@ module.exports = function(db) {
 	// CookieParser should be above session
 	app.use(cookieParser());
 
+	// was causing bug explained here http://stackoverflow.com/questions/22698661/mongodb-error-setting-ttl-index-on-collection-sessions
+	// commented out for now
+
 	// Express MongoDB session storage
-	app.use(session({
-		saveUninitialized: true,
-		resave: true,
-		secret: config.sessionSecret,
-		store: new mongoStore({
-			db: db.connection.db,
-			collection: config.sessionCollection
-		})
-	}));
+	// app.use(session({
+	// 	saveUninitialized: true,
+	// 	resave: true,
+	// 	secret: config.sessionSecret,
+	// 	store: new mongoStore({
+	// 		db: db.connection.db,
+	// 		collection: config.sessionCollection
+	// 	})
+	// }));
 
 	// use passport session
 	app.use(passport.initialize());

commit 7edff7b6f6960b530ecc1aaf8bd086cf2356286f

I had these lines previous commented out as it was causing a bug described at http://stackoverflow.com/questions/22698661/mongodb-error-setting-ttl-index-on-collection-sessions. When these lines are commented out, users do not stay logged in. Persistence between F5 is gone and the user is not "logged in" if they go to reset their password among other processes. This bug was caused in commit 4ebe829 and I am reverse-committing into develop to fix the bug.

config/express.js
@@ -88,19 +88,19 @@ module.exports = function(db) {
 	// CookieParser should be above session
 	app.use(cookieParser());
 
-	// was causing bug explained here http://stackoverflow.com/questions/22698661/mongodb-error-setting-ttl-index-on-collection-sessions
-	// commented out for now
-	
+	// May cause bug explained here http://stackoverflow.com/questions/22698661/mongodb-error-setting-ttl-index-on-collection-sessions
+	// had it commented out but caused login issues
+
 	// Express MongoDB session storage
-	// app.use(session({
-	// 	saveUninitialized: true,
-	// 	resave: true,
-	// 	secret: config.sessionSecret,
-	// 	store: new mongoStore({
-	// 		db: db.connection.db,
-	// 		collection: config.sessionCollection
-	// 	})
-	// }));
+	app.use(session({
+		saveUninitialized: true,
+		resave: true,
+		secret: config.sessionSecret,
+		store: new mongoStore({
+			db: db.connection.db,
+			collection: config.sessionCollection
+		})
+	}));
 
 	// use passport session
 	app.use(passport.initialize());