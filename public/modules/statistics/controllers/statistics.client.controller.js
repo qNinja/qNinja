cff3ad61a9d2929f4b13c199b8ea322843a85a59
Ben Matheson <ben@linux-pxl8.site>
2015-01-28 22:57:11 -0700

    Added basic functionality for dummy data. Still need to fix dependencies.

--- public/modules/statistics/controllers/statistics.client.controller.js
+++ public/modules/statistics/controllers/statistics.client.controller.js
@@ -2,6 +2,38 @@
 
 angular.module('statistics').controller('StatisticsController', ['$scope',
 	function($scope) {
-		$scope.names=['Ben', 'Peter', 'Mark'];
+		$scope.users = [
+		{
+			'x' : 'ben',
+			'y' : [50, 60]
+		},
+		{
+			'x' : 'peter',
+			'y' : [47, 78]
+		},
+		{
+			'x' : 'mark',
+			'y' : [32, 65]
+		}
+		];
+
+		$scope.config = {
+			title: 'Team Statistics',
+			tooltips: true,
+			labels: false,
+			//mouseover: function() {},
+			//mouseout: function() {},
+			//click: function() {},
+			legend: {
+				display: true,
+				//could be 'left, right'
+				position: 'right'
+			}
+		};
+
+		$scope.data = {
+			series: ['open', 'closed'],
+			data: $scope.users
+		};
 	}
 ]);
